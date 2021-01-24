6c5599ac79583730aab302e87e40be34e3d63c76
matteoferla <matteo.ferla@gmail.com>
2021-01-24 17:31:54 +0000

    :pencil: requiremnet error

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 
 requires = [
+    'Flask',
     'Adafruit-Blinka', # circuitpython: 'board', 'digitalio', 'pulseio'
-    'mpu6050', #gyro
-    'pyttsx3'
+    'mpu6050-raspberrypi', #gyro
+    'pyttsx3',
+    #'smbus'
 ]
 
 long_description = open(os.path.join(here, 'README.md')).read()
@@ -20,7 +22,7 @@ setup(
     url='https://github.com/matteoferla/pyfurby',
     license='MIT',
     author='matteoferla',
-    author_email='',
+    author_email='matteo.ferla@gm',
     description='Controlling the various parts of the Furby with Python and a Pi Zero',
     long_description=long_description,
     install_requires=requires
