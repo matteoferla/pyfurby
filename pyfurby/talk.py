467a5cbdcf4203a8d00de4d4986f051e36a4f737
matteoferla <matteo.ferla@gmail.com>
2021-01-24 17:32:47 +0000

    :beetle: with I2S specifying driver is required??

--- pyfurby/talk.py
+++ pyfurby/talk.py
@@ -2,7 +2,7 @@ import pyttsx3
 
 class FurbyTalk:
     def __init__(self, voice_name:str='en-scottish+m4', voice_volume:int=0.7, voice_rate:int=200):
-        self.engine = pyttsx3.init()
+        self.engine = pyttsx3.init('espeak', debug=True)
         self.volume = voice_volume
         self.engine.setProperty('voice', voice_name)
         self.engine.setProperty('rate', voice_rate)
