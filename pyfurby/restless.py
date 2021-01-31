b461a8799a3f832b87c784b7723a3325d9c0f925
matteoferla <matteo.ferla@gmail.com>
2021-01-31 19:37:52 +0000

    :hammer: IP is handy

--- pyfurby/restless.py
+++ pyfurby/restless.py
@@ -15,7 +15,7 @@ class RestlessFurby: # restful...
         """
         try:
             kwargs = request.args
-            print(f'Request {cmd}: {kwargs}')
+            print(f'Request {cmd}: {kwargs} from {request.remote_addr}')
             getattr(self, cmd)(**kwargs)
             return {'status': 'OK'}
         except Exception as error:
@@ -29,7 +29,7 @@ class RestlessFurby: # restful...
         This action is trigged when home is requested.
         """
         reply = '## Furby Restful API options\n\n'
-        reply += 'To trigger a command, say `furby.yell`, use 198.162.1.xx:1998/yell?text=I%20hate%20you ' +\
+        reply += 'To trigger a command, say `furby.yell`, use 198.162.1/0.xx:1998/yell?text=I%20hate%20you ' +\
                  'where xx is the furby\'s netword address\n'
         reply += 'Namely, the route (part before the question mark) is the command, and its arguments are ' +\
                   'key=value separated by an ampersand (that is a URL query).\n' +\
@@ -48,4 +48,4 @@ class RestlessFurby: # restful...
         app = Flask(__name__)
         app.add_url_rule('/<cmd>', 'command', self._resolve_request)
         app.add_url_rule('/', 'home', self._home)
-        app.run(port=1998, host='*')
+        app.run(port=1998, host='0.0.0.0')
